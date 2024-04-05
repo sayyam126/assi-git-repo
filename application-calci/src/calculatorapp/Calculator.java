9f324782288b58a4aa94f978b72145c2acba66bf
saurabhpandey <saurabh5904127@gmail.com>
2024-04-05 11:20:46 +0530

    my first commit

---
+++ application-calci/src/calculatorapp/Calculator.java
@@ -0,0 +1,47 @@
+package calculatorapp;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static int add(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	public static int sub(int number1, int number2) {
+		return (number1 - number2);
+
+	}
+
+	public static int mul(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public static double div(double number1, double number2) {
+		if (number2 == 0) {
+			throw new IllegalArgumentException("Number can not be divide by 0!");
+		}
+
+		return number1 / number2;
+	}
+	
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number : ");
+		int input = sc.nextInt();
+		
+		@SuppressWarnings({ "unused", "resource" })
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Enter Second Number : ");
+		int input1 = sc.nextInt();
+		
+		System.out.println("The Addition is : " + Calculator.add(input, input1));
+		System.out.println("The Substraction is : " + Calculator.sub(input, input1));
+		System.out.println("The Multiplication is : " + Calculator.mul(input, input1));
+		System.out.println("The Division is : " + Calculator.div(input, input1));
+		
+		
+	}
+
+}
